3b2a2f9da11e887cea6fbf18b1ef1ec28ebae7bd
Régis Relland <regis.relland@insee.fr>
2026-03-17 14:27:20 +0100

    modif nom des exports

--- R/install_typst_extension.R
+++ R/install_typst_extension.R
@@ -1,14 +1,14 @@
-#' install_if_typst
+#' install_insee_flash_typst
 #'
 #' Prepare un fichier insee flash pdf à partir d'un fichier .qmd
 #' @export
-install_if_typst <- function() {
+install_insee_flash_typst <- function() {
   # On pointe vers le dossier parent insee flash qui contient TOUT
-  source_dir <- system.file("if", package = "etudes.pdf")
+  source_dir <- system.file("insee-flash", package = "etudes.pdf")
   
   if (source_dir == "") stop("Source introuvable dans le package.")
 
-  dest_dir <- "_extensions/if"
+  dest_dir <- "_extensions/insee-flash"
   
   if (!dir.exists(dest_dir)) {
     dir.create(dest_dir, recursive = TRUE)
@@ -20,22 +20,22 @@ install_if_typst <- function() {
             recursive = TRUE, 
             overwrite = TRUE)
   
-  message("Extension et ressources installées dans _extensions/if")
+  message("Extension et ressources installées dans _extensions/insee-flash")
 }
 
 
 
-#' install_ia_typst
+#' install_insee_analyses_typst
 #'
 #' Prepare un fichier insee analyses pdf à partir d'un fichier .qmd
 #' @export
-install_ia_typst <- function() {
+install_insee_analyses_typst <- function() {
   # On pointe vers le dossier parent insee analyses qui contient TOUT
-  source_dir <- system.file("ia", package = "etudes.pdf")
+  source_dir <- system.file("insee-analyses", package = "etudes.pdf")
   
   if (source_dir == "") stop("Source introuvable dans le package.")
 
-  dest_dir <- "_extensions/ia"
+  dest_dir <- "_extensions/insee-analyses"
   
   if (!dir.exists(dest_dir)) {
     dir.create(dest_dir, recursive = TRUE)
@@ -47,5 +47,5 @@ install_ia_typst <- function() {
             recursive = TRUE, 
             overwrite = TRUE)
   
-  message("Extension et ressources installées dans _extensions/ia")
+  message("Extension et ressources installées dans _extensions/insee-analyses")
 }
